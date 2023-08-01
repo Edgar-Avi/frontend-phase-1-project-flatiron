f089ea9eafb756ffb7a6f7442541f440dc715e0b
Edgar Avila <edgaravila01@yahoo.com>
2023-08-01 15:18:14 -0700

    fetch Api

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-console.log('loaded');
+fetch('https://rickandmortyapi.com/api/character')
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
