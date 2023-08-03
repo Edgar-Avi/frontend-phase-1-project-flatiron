b7bc0437605af0b03700a6a58b050b8e872d06ae
Edgar Avila <edgaravila01@yahoo.com>
2023-08-03 02:11:37 -0700

    add article

--- main.js
+++ main.js
@@ -12,6 +12,19 @@ function getCharacters(rickApi) {
 getCharacters(data => {
    data.results.forEach(characters => {
 
-    const article = document.createRange().createContextualFragment()
+    const article = document.createRange().createContextualFragment( /*html*/`
+    <article>
+
+        <div class="image-container">
+            <img src="https://rickandmortyapi.com/api/character/avatar/2.jpeg" alt="Character"></img>
+        </div>
+
+        <h2>Character</h2>
+        <span>Character status</span>
+
+    </article>
+    `);
+
+
    });
 });
